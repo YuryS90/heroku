1923c7986db8068218ecb4be800e143edf60fa8b
Yury <59015519+YuryS90@users.noreply.github.com>
2020-04-03 17:42:37 +0300

    Goust

--- index.php
+++ index.php
@@ -1,17 +1,62 @@
+<?php
+$link = mysqli_connect("remotemysql.com", "fk5gNJOjoi", "bacElk3OeN")
+    or die("Не удалось соединиться с сервером");
+
+mysqli_select_db($link, "fk5gNJOjoi")
+    or die("Не удалось выбрать БД");
+
+if (!empty($_POST['text'] && !empty($_POST['name']))) {
+    mysqli_query(
+        $link,
+        "INSERT INTO fk5 VALUES(NULL, '$_POST[text]', '$_POST[name]')"
+    );
+    header('Location: index.php');
+};
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Guest Book</title>
+    <title>Document</title>
 </head>
+
 <body>
     <?php
+    //   Username: fk5gNJOjoi
+    //   Database name: fk5gNJOjoi
+    //   Password: bacElk3OeN
+    //   Server: remotemysql.com
+    //   Port: 3306
 
-    for($i = 1; $i <= 10; $i++) {
-        echo"$i<br>";
+    // mysqli_query("SET NAMES 'cp1251'");
+
+    $query = "SELECT * FROM book";
+    $result = mysqli_query($link, $query)
+        or die("Не удалось выполнить запрос");
+
+    echo "<table border='1'>\n";
+    while ($row = mysqli_fetch_array($result, MYSQLI_BOTH)) {
+        echo "<tr>";
+        echo "<td>" . $row[0] . "</td>";
+        echo "<td>" . $row[1] . "</td>";
+        echo "<td>" . $row[2] . "</td>";
+        echo "</tr>";
     }
-    echo("hello World");
+
+    echo  "</table>\n";
+
+    mysqli_free_result($result);
+    mysqli_close($link);
     ?>
+
+    <form action="" method="POST">
+        <textarea name="text" cols="30" rows="10"></textarea>
+        <input type="text" name="name">
+        <input type="submit" value="ok">
+    </form>
 </body>
+
 </html>
